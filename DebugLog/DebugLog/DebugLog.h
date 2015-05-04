0a1be468c8ab9a4d2939ef3a35f1e1ce63edb208
liuyufeng <lyf13629369104@163.com>
2015-05-04 16:01:12 +0800

    封装使用方法

--- DebugLog/DebugLog/DebugLog.h
+++ DebugLog/DebugLog/DebugLog.h
@@ -1,13 +1,31 @@
 //
 //  DebugLog.h
-//  DebugLog
+//  PM25
 //
-//  Created by liuyufeng on 14/12/19.
-//  Copyright (c) 2014年 liuyufeng. All rights reserved.
+//  Created by lyf on 15/4/18.
+//  Copyright (c) 2015年 liuyufeng. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 
+//日志信息结构体
+typedef enum selected
+{
+    WriteDebugLogToFile,          //写日志文件，本地看不到日志
+    LocalDebugLog,                //本地日志模式
+    OnlyWriteErrorLogToFile,      //只写崩溃日志到文件，本地显示日志
+    ReleaseNoLog                  //发布模式，不写日志文件
+}LogLel;
+
+
+
+
 @interface DebugLog : NSObject
 
+
++ (void)setDebug:(LogLel)selected;
+
+
 @end
+
+
--- DebugLog/DebugLog/DebugLog.m
+++ DebugLog/DebugLog/DebugLog.m
@@ -1,13 +1,63 @@
 //
 //  DebugLog.m
-//  DebugLog
+//  PM25
 //
-//  Created by liuyufeng on 14/12/19.
-//  Copyright (c) 2014年 liuyufeng. All rights reserved.
+//  Created by lyf on 15/4/18.
+//  Copyright (c) 2015年 liuyufeng. All rights reserved.
 //
 
 #import "DebugLog.h"
+#import "UncaughtExceptionHandler.h"
+#import "CinLoggerManager.h"
+#import "SandBoxPath.h"
 
 @implementation DebugLog
 
+
++ (void)setDebug:(LogLel)selected;
+{
+    switch (selected)
+    {
+        case WriteDebugLogToFile:
+        {
+            InstallUncaughtExceptionHandler();
+#define LOG(fmt, ...) do {NSString* file = [[NSString alloc] initWithFormat:@"%s", __FILE__]; NSLog((@"%@(%d)%s " fmt), [file lastPathComponent], __LINE__, ##__VA_ARGS__,__func__);} while(0)
+            //写日志
+            //写日志到文件
+            CinLoggerManager * logShare = [CinLoggerManager shareCinLoggerManager];
+            [logShare writeLogToSandBoxFile];
+            
+            break;
+        }
+        case LocalDebugLog:
+        {
+#define LOG(fmt, ...) do {NSString* file = [[NSString alloc] initWithFormat:@"%s", __FILE__]; NSLog((@"%@(%d)%s " fmt), [file lastPathComponent], __LINE__, ##__VA_ARGS__,__func__);} while(0)
+            break;
+        }
+        case OnlyWriteErrorLogToFile:
+        {
+            InstallUncaughtExceptionHandler();
+#define LOG(fmt, ...) do {NSString* file = [[NSString alloc] initWithFormat:@"%s", __FILE__]; NSLog((@"%@(%d)%s " fmt), [file lastPathComponent], __LINE__, ##__VA_ARGS__,__func__);} while(0)
+            break;
+        }
+        case ReleaseNoLog:
+        {
+#define NSLog(format,...)
+#define LOG(...)
+            break;
+        }
+        default:
+        {
+            
+            break;
+        }
+    }
+    
+    
+}
+
 @end
+
+
+
+
